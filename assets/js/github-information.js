ae329a6a1e295ec9d2ade8aed19e419fc03ae3c7
jeremyhsimons <jeremysimons15@gmail.com>
2023-04-18 10:58:15 +0000

    Add feature to display searched user's repos on the github page.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -18,6 +18,29 @@ function userInformationHTML(user) {
     </div>`
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length === 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+    const listItemsHTML = repos.map(function(repo) {
+        return `
+        <li>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>
+        `
+    });
+
+    return `
+    <div class="clearfix repo-list">
+        <p>
+            <strong>Repo List:</strong>
+        </p>
+        <ul>
+            ${listItemsHTML.join("\n")}
+        </ul>
+    </div>`
+}
+
 function fetchGitHubInformation(event) {
     const username = $("#gh-username").val();
     if (!username) {
@@ -32,11 +55,14 @@ function fetchGitHubInformation(event) {
     )
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            const userData = response;
+        function(firstResponse, secondResponse) {
+            const userData = firstResponse[0];
+            const repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
